98e5bc2aa537ea04199bb858e6fcad8708ffa7a7
laura97torres <laurita-021@hotmail.es>
2020-05-05 23:25:23 -0500

    home y mcdelivery terminados

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,10 +1,22 @@
 
 import HomePage from '../pages/home.f7.html';
+import mcdelivery from '../pages/mcdelivery.f7.html';
+import cupones from '../pages/cupones.f7.html';
 
 var routes = [
   {
     path: '/',
     component: HomePage,
+  },
+
+  {
+    path: '/mcdelivery/',
+    component: mcdelivery,
+  },
+
+  {
+    path: '/cupones/',
+    component: cupones,
   },
 ];
 
