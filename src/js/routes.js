b988974669c5d854addf16b868615204a531d24d
laura97torres <laurita-021@hotmail.es>
2020-05-06 23:01:22 -0500

    pag cupones y tour

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,6 +2,9 @@
 import HomePage from '../pages/home.f7.html';
 import mcdelivery from '../pages/mcdelivery.f7.html';
 import cupones from '../pages/cupones.f7.html';
+import miscupones from '../pages/miscupones.f7.html';
+import tourdenuestracocina from '../pages/tourdenuestracocina.f7.html';
+
 
 var routes = [
   {
@@ -17,6 +20,16 @@ var routes = [
   {
     path: '/cupones/',
     component: cupones,
+  },
+
+  {
+    path: '/tourdenuestracocina/',
+    component: tourdenuestracocina,
+  },
+
+  {
+    path: '/miscupones/',
+    component: miscupones,
   },
 ];
 
